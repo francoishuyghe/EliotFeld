6b7c0cdf35f42560f0e27eaac04493c2dffcb331
Francois Huyghe <francois@13milliseconds.com>
2021-11-22 11:30:24 -0500

    Maggie Notes

--- web/app/themes/eliotfeld-2021/resources/views/partials/content-single-ballet.blade.php
+++ web/app/themes/eliotfeld-2021/resources/views/partials/content-single-ballet.blade.php
@@ -7,19 +7,24 @@
   </header>
   <div class="entry-content">
     <h2>General Info</h2>
-    @if( $general_info['running_time']) <p>Running Time: {{ $general_info['running_time'] }}min</p> @endif
-    <p>This ballet is @if($general_info['running_time']) in @else <b>not in</b> @endif the public domain.</p>
+    @if( $general_info['running_time']) <p>Running Time: {{ $general_info['running_time'] }} min</p> @endif
 
-    <h2>Key People</h2>
+    <h2>Creative Team</h2>
     @if($data['lighting_designer'])<p>Lighting Designer: {{ $data['lighting_designer']->post_title }}</p>@endif
     @if($data['costume_designer'])<p>Costume Designer: {{ $data['costume_designer']->post_title }}</p>@endif
     @if($data['set_designer'])<p>Set Designer: {{ $data['set_designer']->post_title }}</p>@endif
+
+    {{-- MUSIC --}}
+    @php $music = $data['music'] @endphp
+    @if( $music['composer'])<p>Composer: {{ $music['composer']->post_title }}</p> @endif
+    @if( $music['composition'])<p>Composition: {{ $music['composition'] }}</p> @endif
+    @if( $music['music_notes'])<h3>Music Notes</h3> {!! $music['music_notes'] !!} @endif
 
     <h2>Premiere</h2>
     @php $premiere = $data['premiere'] @endphp
     @if( $premiere['date'])<p>Date: {{ $premiere['date'] }}</p> @endif
     @if( $premiere['location'])<p>Location: {{ $premiere['location'] }}</p> @endif
-    @if( $premiere['company'])<p>Company {{ $premiere['company'] }}</p> @endif
+    @if( $premiere['company'])<p>Company: {{ $premiere['company'] }}</p> @endif
     @if( $premiere['cast'])
     <ul>
       @foreach ($premiere['cast'] as $castMember)
@@ -32,12 +37,8 @@
     <h2>Notes</h2>
     {!! $data['notes'] !!}
     @endif
-    
-    <h2>Music</h2>
-    @php $music = $data['music'] @endphp
-    @if( $music['composer'])<p>Composer: {{ $music['composer']->post_title }}</p> @endif
-    @if( $music['composition'])<p>Composition: {{ $music['composition'] }}</p> @endif
-    @if( $music['music_notes'])<h3>Notes</h3> {!! $music['music_notes'] !!} @endif
+
+    <p>This ballet is @if($general_info['running_time']) in @else <b>not in</b> @endif the public domain.</p>
     
     <h2>Media</h2>
     @php $media = $data['media'] @endphp
@@ -61,8 +62,9 @@
     </div>
     @endif
   </div>
+
   <footer>
-    {!! wp_link_pages(['echo' => 0, 'before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']) !!}
+    
   </footer>
   </div>
 </article>
