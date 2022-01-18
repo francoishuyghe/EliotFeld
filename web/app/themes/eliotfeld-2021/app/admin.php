2f42ff43dd309f863034785bbf20544bddfd3ec0
Francois Huyghe <francois@13milliseconds.com>
2022-01-18 10:34:39 -0500

    Add last updated column

--- web/app/themes/eliotfeld-2021/app/admin.php
+++ web/app/themes/eliotfeld-2021/app/admin.php
@@ -22,3 +22,27 @@ add_action('customize_register', function (\WP_Customize_Manager $wp_customize) 
 add_action('customize_preview_init', function () {
     wp_enqueue_script('sage/customizer.js', asset_path('scripts/customizer.js'), ['customize-preview'], null, true);
 });
+
+// Register the column
+function modified_column_register( $columns ) {
+    $columns['modified_list'] = __( 'Modified', 'my-plugin' );
+  
+    return $columns;
+}
+add_filter( 'manage_edit-post_columns', 'modified_column_register' );
+ 
+// Display the column content
+function modified_column_display( $column_name, $post_id ) {
+    if ( 'modified_list' != $column_name )
+        return;
+    echo the_modified_date();
+}
+add_action( 'manage_posts_custom_column', 'modified_column_display', 10, 2 );
+ 
+// Register the column as sortable
+function modified_column_register_sortable( $columns ) {
+    $columns['modified_list'] = 'modified_list';
+  
+    return $columns;
+}
+add_filter( 'manage_edit-post_sortable_columns', 'modified_column_register_sortable' );
