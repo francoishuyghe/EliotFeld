4bdcb1f6ddfbb717be7283910a158080b6ac5d88
Francois Huyghe <francois@13milliseconds.com>
2022-03-16 11:47:52 -0400

    add home image credit

--- web/app/themes/eliotfeld-2021/resources/views/partials/content-page-home.blade.php
+++ web/app/themes/eliotfeld-2021/resources/views/partials/content-page-home.blade.php
@@ -1,4 +1,5 @@
 <div class="container">
     <h1>{{ the_title() }}</h1>
     <img class="eliot-cutout" src="@asset('images/eliot-cutout.png')" />
+    <div class="copyright">Jack Mitchell via Getty Images</div>
 </div>
